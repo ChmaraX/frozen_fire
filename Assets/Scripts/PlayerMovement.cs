c2b884366252cbf1814af2dcb363f9d85828b924
Adam Chmara <adam.chmara1@gmail.com>
2021-11-14 17:40:35 +0100

    Add player crouch/slide; interrupt jump with crouch

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,36 +5,82 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private PolygonCollider2D coll;
-    private SpriteRenderer sprite;
+    private BoxCollider2D coll;
+    private SpriteRenderer spriteRenderer;
     private Animator animator;
 
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
+
     [SerializeField] private float jumpForce = 14f;
+    [SerializeField] private float crouchForce = 14f;
+
+    private Vector2 standingSize;
+    private Vector2 standingOffset;
+
+    private bool isCrouching;
+    [SerializeField] private Vector2 crouchingSize;
+    [SerializeField] private Vector2 crouchingOffset;
+
     [SerializeField] private LayerMask jumpableGround;
 
-    private enum MovementState { idle, running, jumping, falling, dash }
+    private enum MovementState { idle, running, jumping, falling, crouching }
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        coll = GetComponent<PolygonCollider2D>();
-        sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+
+        standingSize = coll.size;
+        standingOffset = coll.offset;
+
+        crouchingSize = new Vector2(standingSize.x, standingSize.y * 0.5f);
+        crouchingOffset = new Vector2(crouchingOffset.x, -1);
     }
 
     // Update is called once per frame
     void Update()
     {
-
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
-        
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+
+        // movement possible on ground
+        if (IsGrounded())
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            if (Input.GetButtonDown("Jump"))
+            {
+                Jump();
+            }
+
+            // crouching
+            if (Input.GetButtonDown("Crouch"))
+            {
+                Crouch();
+            }
+
+            // stop crouching
+            if (Input.GetButtonUp("Crouch"))
+            {
+                StopCrouch();
+            }
+        }
+        // movement possible in air
+        else
+        {
+            // falling down faster
+            if (Input.GetButtonDown("Crouch"))
+            {
+                Down();
+            }
+
+            // prevent crouching in air
+            if (Input.GetButtonDown("Jump"))
+            {
+                StopCrouch();
+            }
         }
 
         UpdateAnimationState();
@@ -42,20 +88,19 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateAnimationState()
     {
-
         MovementState state;
 
         // if moving to right
         if (dirX > 0f)
         {
             state = MovementState.running;
-            sprite.flipX = false;
+            spriteRenderer.flipX = false;
         }
         // if moving to left (we prob. don't need this) 
         else if (dirX < 0f)
         {
             state = MovementState.running;
-            sprite.flipX = true;
+            spriteRenderer.flipX = true;
         }
         // not moving
         else
@@ -63,18 +108,17 @@ public class PlayerMovement : MonoBehaviour
             state = MovementState.idle;
         }
 
-        // dash-crouch
-        if (Input.GetKeyDown("s") && IsGrounded())
+        // slide-crouch
+        if (isCrouching)
         {
-            // TODO: make collider take less space
-            state = MovementState.dash;
+            state = MovementState.crouching;
         }
 
         // jumping and falling has the highest priority so it can overwrite state
         if (rb.velocity.y > 0.1f)
         {
             state = MovementState.jumping;
-        } 
+        }
         else if (rb.velocity.y < -0.1f)
         {
             state = MovementState.falling;
@@ -88,5 +132,29 @@ public class PlayerMovement : MonoBehaviour
         // checks if box around player is overlapping "jumpableGround" (terrain which has mask Ground)
         // box around player is moved by .2f down so overlap is possible
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .2f, jumpableGround);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void Down()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, -crouchForce);
+    }
+
+    private void Crouch()
+    {
+        isCrouching = true;
+        coll.size = crouchingSize;
+        coll.offset = crouchingOffset;
+    }
+
+    private void StopCrouch()
+    {
+        isCrouching = false;
+        coll.size = standingSize;
+        coll.offset = standingOffset;
     }
 }
