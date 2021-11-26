ff2d751103d5eef7870033807762a5b96505d2c6
lukas <l.misaga@gmail.com>
2021-11-26 23:41:03 +0100

    Projectile lifetime, tweaked player running animation

--- Assets/Scripts/ProjectileBehaviour.cs
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class ProjectileBehaviour : MonoBehaviour
 {
+
+    public readonly float defaultProjectileLifetime = 3f;
+
     public float speed = 4.5f;
+    [SerializeField] private float projectileLife;
+
+    void Start()
+    {
+        //initialize projectile with default projectile time
+        projectileLife = defaultProjectileLifetime;
+    }
 
     void Update()
     {
         transform.position += transform.right * Time.deltaTime * speed;
+
+        //update projectile life time, destroy it if the lifetime runs out
+        projectileLife -= Time.deltaTime;
+        if (projectileLife <= 0f) {
+            Destroy(gameObject);
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
