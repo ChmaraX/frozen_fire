e2c4197c3ade8e81f5b2c39cbf1b9fb4454bbc9f
lukas <l.misaga@gmail.com>
2021-11-26 21:11:13 +0100

    Added coin collector text, custom fonts

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
 
-    private int collectedCherries = 0;
+    private int collectedCoins = 0;
+
+    [SerializeField] private Text coinsText; 
 
     private void OnTriggerEnter2D(Collider2D collider) 
     {
         if (collider.gameObject.CompareTag("Coin")) 
         {
-            collectedCherries++;
+            collectedCoins++;
             Destroy(collider.gameObject);
-            Debug.Log("collected cherry: " + collectedCherries);
+            coinsText.text = collectedCoins.ToString();
         }
     }
 }
