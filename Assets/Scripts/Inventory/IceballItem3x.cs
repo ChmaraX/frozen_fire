39abd181febd667060cc446ad37a48444b7dbc71
Adam Chmara <adam.chmara1@gmail.com>
2021-12-14 21:31:32 +0100

    Add Iceball x3 item

---
+++ Assets/Scripts/Inventory/IceballItem3x.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class IceballItem3x : MonoBehaviour, IInventoryItem
+{
+    public string Name
+    {
+        get
+        {
+            return "Iceball";
+        }
+    }
+
+    public Sprite _Image = null;
+
+    public Sprite Image
+    {
+        get
+        {
+            return _Image;
+        }
+    }
+
+    public InventorySlot _Slot = null;
+
+    public InventorySlot Slot
+    {
+        get
+        {
+            return _Slot;
+        }
+        set
+        {
+            _Slot = value;
+        }
+    }
+
+    public bool IsStorable => true;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnUse()
+    {
+        Debug.Log("Item Iceball3x was used");
+    }
+}
