f0590db536d82ff8639511bef1dc480cbe369847
lukas <l.misaga@gmail.com>
2021-12-16 23:35:40 +0100

    Disable incomplete levels in level selector (test)

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,10 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
+
+    public Button[] levelEntries;
+
+    void Start() 
+    {
+        int levelReached = PlayerPrefs.GetInt("LastReachedLevel", 1);
+
+        for (int i = 0; i < levelEntries.Length; i++) 
+        {
+            Debug.Log(levelEntries[i]);
+            
+            if (i+1 > levelReached) 
+            {
+             levelEntries[i].interactable = false;
+            }
+        }
+    }
 
     public void SelectSceneByTitle(string levelSceneTitle) 
     {
