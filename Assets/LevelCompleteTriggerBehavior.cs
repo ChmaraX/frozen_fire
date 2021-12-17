80fc3bdade83727cd22b988dae9642b38501aa13
lukas <l.misaga@gmail.com>
2021-12-17 15:50:24 +0100

    Save level progress, logic for improving saved level data

--- Assets/LevelCompleteTriggerBehavior.cs
+++ Assets/LevelCompleteTriggerBehavior.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelCompleteTriggerBehavior : MonoBehaviour
@@ -12,8 +10,8 @@ public class LevelCompleteTriggerBehavior : MonoBehaviour
         //find out if player
         if (collider.gameObject.CompareTag("Player"))
         {
-            this.UnlockNewLevel();
             ItemCollector itemCollector = collider.gameObject.GetComponent<ItemCollector>();
+            this.SaveProgress(itemCollector);
             collider.gameObject
                 .GetComponent<Rigidbody2D>()
                 .constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
@@ -24,14 +22,26 @@ public class LevelCompleteTriggerBehavior : MonoBehaviour
         }
     }
 
-    private void UnlockNewLevel() 
+    private void SaveProgress(ItemCollector itemCollector) 
     {
         int savedUnlockedLevel = PlayerPrefs.GetInt("lastUnlockedLevel");
+        int previousCollectedCoins = PlayerPrefs.GetInt(levelToUnlock-1 +"_coins", 0);
+        int previousDeaths = PlayerPrefs.GetInt(levelToUnlock-1 +"_deaths", 0);
+
+        //check if levelToUnlock is not already unlocked
         if (savedUnlockedLevel < levelToUnlock) 
         {
-             PlayerPrefs.SetInt("lastUnlockedLevel", levelToUnlock);
-            PlayerPrefs.Save();
-        } 
+            PlayerPrefs.SetInt("lastUnlockedLevel", levelToUnlock);
+        }
+
+        //check if current coins collected & deaths are better than saved 
+        if (previousCollectedCoins <= itemCollector.collectedCoins && previousDeaths >= itemCollector.deathCount) 
+        {
+            PlayerPrefs.SetInt(levelToUnlock-1 + "_coins", itemCollector.collectedCoins);
+            PlayerPrefs.SetInt(levelToUnlock-1 + "_deaths", itemCollector.deathCount);
+        }
+
+        PlayerPrefs.Save();
     }
 
 }
