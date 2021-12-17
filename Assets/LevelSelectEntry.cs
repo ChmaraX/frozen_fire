46a8b4d242033a760c34df9a001ccbaa97acdfa6
lukas <l.misaga@gmail.com>
2021-12-17 19:04:04 +0100

    Dynamic level info in level select

---
+++ Assets/LevelSelectEntry.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class LevelSelectEntry : MonoBehaviour
+{
+    public TextMeshProUGUI coinsText;
+
+    public TextMeshProUGUI deathsText;
+    public TextMeshProUGUI levelNumberText;
+
+    public int levelNumber;
+
+
+    public void SetLevelData(int coins, int deaths) 
+    {
+        coinsText.text = coins.ToString();
+        deathsText.text = deaths.ToString();
+        levelNumberText.text = levelNumber.ToString();
+    }
+}
--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -10,9 +10,17 @@ public class LevelSelector : MonoBehaviour
     void Start() 
     {
         int lastUnlockedLevel = PlayerPrefs.GetInt("lastUnlockedLevel", 1) ;
+        
+        // Debug.Log(levelEntries[i].GetComponentsInChildren<Text>());
 
+        // levelEntries = new Button[transform.childCount];
         for (int i = 0; i < levelEntries.Length; i++) 
         {
+            int levelNumber = i+1;
+            int coins = PlayerPrefs.GetInt(levelNumber + "_coins", 0);
+            int deaths = PlayerPrefs.GetInt(levelNumber + "_deaths", 0);
+            levelEntries[i].GetComponentInChildren<LevelSelectEntry>().SetLevelData(coins, deaths);
+            
             //unlock levels that are only one above last completed level
             if (i+1 > lastUnlockedLevel) 
             {
